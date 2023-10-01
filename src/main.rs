3254451813a5e25204ec49c3f240bea03d438603
haijima <k.haijima@gmail.com>
2023-10-01 23:30:44 +0900

    Add function to convert a number into human readable string

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
 fn main() {
-    println!("Hello, world!");
+    println!("{}", human_readable(0));
+    println!("{}", human_readable(550));
+    println!("{}", human_readable(563_200));
+    println!("{}", human_readable(681_574_400));
+    println!("{}", human_readable(123_456_789));
+}
+
+const BASE: f64 = 1024f64;
+const UNITS: [&str; 8] = ["B", "KiB", "MiB", "GiB", "TiB", "PiB", "EiB", "ZiB"];
+
+pub fn human_readable<T: Into<f64>>(bytes: T) -> String {
+    let mut size = bytes.into();
+    for unit in UNITS {
+        if size < BASE {
+            let s = format!("{:.1}", size)
+                .trim_end_matches(".0")
+                .to_string();
+            return format!("{} {}", s, unit);
+        }
+        size /= BASE;
+    }
+    format!("{:.1} {}", size, "YiB")
 }
