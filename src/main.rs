4e2f0c7832609adf216282cb36132226f6f37c02
haijima <k.haijima@gmail.com>
2023-10-19 18:23:01 +0900

    Change print format

--- src/main.rs
+++ src/main.rs
@@ -60,17 +60,19 @@ fn read<R: BufRead>(buf_reader: R, delimiter: &str, fields: u8) {
 }
 
 const BASE: f64 = 1024f64;
-// const UNITS: [&str; 8] = ["B", "KiB", "MiB", "GiB", "TiB", "PiB", "EiB", "ZiB"];
-const UNITS: [&str; 8] = ["B", "K", "M", "G", "T", "P", "E", "Z"];
+// const UNITS: [&str; 7] = ["KiB", "MiB", "GiB", "TiB", "PiB", "EiB", "ZiB"];
+const UNITS: [&str; 7] = ["K", "M", "G", "T", "P", "E", "Z"];
 
-pub fn human_readable<T: Into<u32>>(bytes: T) -> String {
+pub fn human_readable<T: Into<u128>>(bytes: T) -> String {
     let mut size = bytes.into() as f64;
-    for unit in UNITS {
-        if size < BASE {
-            let s = format!("{:.1}", size).trim_end_matches(".0").to_string();
-            return format!("{}{}", s, unit);
-        }
-        size /= BASE;
+    if size < BASE {
+        return format!("{}{}", size, "B");
     }
-    format!("{:.1}{}", size, "Y")
+    for unit in UNITS {
+        size /= BASE;
+        if size < BASE {
+            return format!("{:.1}{}", size, unit);
+        }
+    }
+    format!("{:.1}{}", size / BASE, "Y")
 }
