33a49441e104ce45dbf80209705fca6d7ab853da
haijima <k.haijima@gmail.com>
2023-10-24 22:38:19 +0900

    Add new flags

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use clap::Parser;
-use clap_verbosity_flag::{InfoLevel, Verbosity};
+use clap::{Parser, ValueEnum};
+use std::fmt::{Display, Formatter, Result};
 use std::fs::File;
 use std::io::{stdin, BufRead, BufReader};
 
@@ -9,9 +9,6 @@ struct Cli {
     #[arg(value_name = "FILE", help = "File to read, if empty read from stdin")]
     filename: Option<String>,
 
-    #[command(flatten)]
-    verbose: Verbosity<InfoLevel>,
-
     /// Use <DELIMITER> as the field delimiter
     #[arg(short, long, default_value = "\t")]
     delimiter: String,
@@ -19,6 +16,34 @@ struct Cli {
     /// Specify which fields to convert to human-readable format
     #[arg(short, long, default_value = "1")]
     fields: usize,
+
+    /// Specify which unit to use
+    #[arg(short, long, value_enum, default_value_t = Unit::Auto)]
+    unit: Unit,
+
+    /// Decimal precision of the output
+    #[arg(short, long, default_value = "1")]
+    precision: usize,
+}
+
+#[derive(Debug, Clone, Default, PartialEq, ValueEnum)]
+pub enum Unit {
+    #[default]
+    Auto,
+    Byte,
+    Kilo,
+    Mega,
+    Giga,
+    Tera,
+    Peta,
+    Exa,
+}
+
+impl Display for Unit {
+    fn fmt(&self, f: &mut Formatter) -> Result {
+        let raw = format!("{:?}", self);
+        write!(f, "{}", raw.to_uppercase())
+    }
 }
 
 fn main() {
@@ -31,10 +56,17 @@ fn main() {
             Err(err) => return eprintln!("{}", err), // Print error and exit if file cannot be opened
         },
     };
-    read(buf_reader, &cli.delimiter, cli.fields);
+
+    read(
+        buf_reader,
+        &cli.delimiter,
+        cli.fields,
+        &cli.unit,
+        cli.precision,
+    );
 }
 
-fn read<R: BufRead>(buf_reader: R, delimiter: &str, fields: usize) {
+fn read<R: BufRead>(buf_reader: R, delimiter: &str, fields: usize, unit: &Unit, precision: usize) {
     for line in buf_reader.lines() {
         println!(
             "{}",
@@ -44,7 +76,7 @@ fn read<R: BufRead>(buf_reader: R, delimiter: &str, fields: usize) {
                 .map(|(i, c)| {
                     if i + 1 == fields {
                         match c.parse::<u64>() {
-                            Ok(n) => human_readable(n),
+                            Ok(n) => human_readable(n, unit, precision),
                             Err(_) => c.to_string(),
                         }
                     } else {
@@ -59,13 +91,28 @@ fn read<R: BufRead>(buf_reader: R, delimiter: &str, fields: usize) {
 
 const UNITS: [&str; 7] = ["B", "K", "M", "G", "T", "P", "E"];
 
-pub fn human_readable<T: Into<u64>>(bytes: T) -> String {
+pub fn human_readable<T: Into<u64>>(bytes: T, unit: &Unit, precision: usize) -> String {
     let size = bytes.into() as f64;
-    let i = size.log(1024_f64).floor() as usize;
+    let i = match unit {
+        Unit::Byte => 0,
+        Unit::Kilo => 1,
+        Unit::Mega => 2,
+        Unit::Giga => 3,
+        Unit::Tera => 4,
+        Unit::Peta => 5,
+        Unit::Exa => 6,
+        Unit::Auto => size.log(1024_f64).floor() as usize,
+    };
+
     if i == 0 {
         return format!("{}{}", size, "B");
     }
-    format!("{:.1}{}", size / (1u64 << (10 * i)) as f64, UNITS[i])
+    return format!(
+        "{:.prec$}{}",
+        size / (1u64 << (10 * i)) as f64,
+        UNITS[i],
+        prec = precision
+    );
 }
 
 #[cfg(test)]
@@ -74,17 +121,24 @@ mod tests {
 
     #[test]
     fn test_human_readable() {
-        assert_eq!(human_readable(0u8), "0B");
-        assert_eq!(human_readable(1u8), "1B");
-        assert_eq!(human_readable(1023u32), "1023B");
-        assert_eq!(human_readable(1024u32), "1.0K");
-        assert_eq!(human_readable(1025u32), "1.0K");
-        assert_eq!(human_readable(1048576u32), "1.0M");
-        assert_eq!(human_readable(1073741824u64), "1.0G");
-        assert_eq!(human_readable(1200000000u64), "1.1G");
-        assert_eq!(human_readable(1099511627776u64), "1.0T");
-        assert_eq!(human_readable(1125899906842624u64), "1.0P");
-        assert_eq!(human_readable(1152921504606846976u64), "1.0E");
-        assert_eq!(human_readable(u64::MAX), "16.0E");
+        assert_eq!(human_readable(0u8, &Unit::Auto, 1), "0B");
+        assert_eq!(human_readable(1u8, &Unit::Auto, 1), "1B");
+        assert_eq!(human_readable(1023u32, &Unit::Auto, 1), "1023B");
+        assert_eq!(human_readable(1024u32, &Unit::Auto, 1), "1.0K");
+        assert_eq!(human_readable(1025u32, &Unit::Auto, 1), "1.0K");
+        assert_eq!(human_readable(1048576u32, &Unit::Auto, 1), "1.0M");
+        assert_eq!(human_readable(1073741824u64, &Unit::Auto, 1), "1.0G");
+        assert_eq!(human_readable(1200000000u64, &Unit::Auto, 1), "1.1G");
+        assert_eq!(human_readable(1099511627776u64, &Unit::Auto, 1), "1.0T");
+        assert_eq!(human_readable(1125899906842624u64, &Unit::Auto, 1), "1.0P");
+        assert_eq!(
+            human_readable(1152921504606846975u64, &Unit::Auto, 1),
+            "1.0E"
+        );
+        assert_eq!(
+            human_readable(1152921504606846976u64, &Unit::Auto, 1),
+            "1.0E"
+        );
+        assert_eq!(human_readable(u64::MAX, &Unit::Auto, 1), "16.0E");
     }
 }
