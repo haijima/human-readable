a6bfb30922f9162f9e719b4b5ab876ad2bbf359f
haijima <k.haijima@gmail.com>
2023-10-31 22:13:11 +0900

    feat: Add logger

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use std::fs::File;
-use std::io::{stdin, BufReader};
+use std::io::{stdin, BufReader, Write};
 
 use clap::Parser;
+use clap_verbosity_flag::Verbosity;
 
 use hr::Unit;
 
@@ -26,10 +27,15 @@ struct Cli {
     /// Decimal precision of the output
     #[arg(short, long, default_value = "1")]
     precision: usize,
+
+    #[clap(flatten)]
+    verbose: Verbosity,
 }
 
 fn main() {
     let cli = Cli::parse();
+
+    init_logger(cli.verbose.log_level_filter());
 
     let buf_reader: BufReader<Box<dyn std::io::Read>> = match cli.filename {
         None => BufReader::new(Box::new(stdin().lock())), // Use stdin if no file is specified
@@ -43,4 +49,18 @@ fn main() {
         buf_reader,
         hr::Config::new(cli.delimiter, cli.fields, cli.unit, cli.precision),
     );
+}
+
+fn init_logger(level_filter: log::LevelFilter) {
+    env_logger::Builder::new()
+        .filter_level(level_filter)
+        .format(|buf, record| {
+            writeln!(
+                buf,
+                "{}: {}",
+                buf.default_styled_level(record.level()),
+                record.args(),
+            )
+        })
+        .init();
 }
